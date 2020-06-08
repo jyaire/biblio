1fd75352b01c295113d8ceec35aed1840b1cb6a6
Jean-Roch Masson <jyaire@gmail.com>
2020-06-08 23:11:35 +0200

    install yarn, webpack encore, bootstrap, and first Home

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,10 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.css';
-
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-import $ from 'jquery';
+import '../css/app.scss';
 
 const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
